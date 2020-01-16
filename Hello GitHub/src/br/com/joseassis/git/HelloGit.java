e2d2a7b1587b9c71be4f34cebee637125219eba6
RannyGoncalves <59902775+RannyGoncalves@users.noreply.github.com>
2020-01-15 22:42:45 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author Asus
+ *Exemplo de integração com o githu
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
